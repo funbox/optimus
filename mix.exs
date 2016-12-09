1b952c8ee6ac2ce373a3125b7c05f873b992f254
av <av@fun-box.ru>
2016-12-09 17:39:18 +0300

    Declared support of Elixir 1.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Optimus.Mixfile do
     [
       app: :optimus,
       version: "0.1.0",
-      elixir: "~> 1.3",
+      elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
