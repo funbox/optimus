9e9ef4294b46ecd884866a0d6e8ab5f3a5706d1f
av <av@fun-box.ru>
2017-03-17 16:13:43 +0300

    Version bumpup

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Optimus.Mixfile do
   def project do
     [
       app: :optimus,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
