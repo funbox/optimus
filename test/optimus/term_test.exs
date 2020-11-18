79314cd2c6a01c0ebe012ea47baec0fad8c8bc1d
av <av@fun-box.ru>
2020-11-18 13:26:19 +0300

    Do not run terminal tests under CI

--- test/optimus/term_test.exs
+++ test/optimus/term_test.exs
@@ -2,8 +2,13 @@ defmodule Optimus.TermTest do
   use ExUnit.Case
 
   test "width" do
-    assert {:ok, n} = Optimus.Term.width()
-    assert is_integer(n)
-    assert n > 0
+    case System.fetch_env("CI") do
+      :error ->
+        assert {:ok, n} = Optimus.Term.width()
+        assert is_integer(n)
+        assert n > 0
+      {:ok, _} ->
+        assert true
+    end
   end
 end
