483787b723d43315b8d465db1a72f985b0aef077
corka149 <corka149@mailbox.org>
2022-02-24 20:23:31 +0100

    Format code

--- test/optimus/term_test.exs
+++ test/optimus/term_test.exs
@@ -2,7 +2,7 @@ defmodule Optimus.TermTest do
   use ExUnit.Case
 
   test "width" do
-    unless System.get_env("CI")  do
+    unless System.get_env("CI") do
       assert {:ok, n} = Optimus.Term.width()
       assert is_integer(n)
       assert n > 0
